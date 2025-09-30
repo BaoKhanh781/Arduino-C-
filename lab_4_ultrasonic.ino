a5cee1d373d485062fffcb5cbc428164ed97b8ae
BaoKhanh781 <152123666+BaoKhanh781@users.noreply.github.com>
2025-09-30 08:55:12 -0700

    Add files via upload

---
+++ lab_4_ultrasonic.ino
@@ -0,0 +1,61 @@
+/*
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+*/
+
+// defines pins numbers
+const int trigPin = 11;
+const int echoPin = 12;
+
+// defines variables
+long duration;
+int distance;
+int n;
+
+long ultrasonic_distance() {
+   // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distancec
+  distance = duration * 0.034 / 2;
+  return distance;
+}
+
+void setup() {
+  // UltraSonoc Pins
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  n = 0;
+  distance = ultrasonic_distance();
+  Serial.println("Waiting for Object");
+  while (distance <= 5){
+    Serial.println("Object Detected & Present");
+    n++;
+    delay(100);
+    distance = ultrasonic_distance();
+    if (distance > 5){
+        Serial.println("Object is Gone");
+        Serial.print("The amount of objects is ");
+        Serial.println(n);
+    }
+  }
+  delay(1000);
+}
